5c42334177543a20343647ba457d7a2ef2e84417
Eduardo-PH <eduardo.ph3@gmail.com>
2022-07-23 12:21:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('db_cadastro', 'postgres', 'eduardo2001', {
     host: 'localhost',
     dialect:'postgres'
   });
+
+sequelize.authenticate().then(function(){
+  console.log("Conectado com sucesso!")
+}).catch(function(erro){
+  console.log("Falha ao se conectar: " + erro)
+})
